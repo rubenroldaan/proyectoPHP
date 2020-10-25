01d72b7dec15a80059b5ae8c42ca9a75b8dd8a0c
Ruben <rubenrolcan@gmail.com>
2020-10-25 12:42:42 +0100

    Línea para crear BD añadida

--- accidentao.sql
+++ accidentao.sql
@@ -7,6 +7,9 @@
 -- Versión del servidor: 10.4.13-MariaDB
 -- Versión de PHP: 7.4.7
 
+
+CREATE DATABASE IF NOT EXISTS accidentao CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
