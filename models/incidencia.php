aafb78ec6f94db1c690ab2f948ad416ab9aeedb0
Ruben <rubenrolcan@gmail.com>
2020-11-06 10:06:55 +0100

    Añado comentarios a los modelos

--- models/incidencia.php
+++ models/incidencia.php
@@ -10,6 +10,7 @@
             $this->db = new mysqli('localhost','root','','accidentao');
         }
 
+
         /**
          * Función que busca una incidencia y si la encuentra la devuelve. Si no la encuentra, devuelve null.
          * @param id el ID de la incidencia a buscar.
@@ -23,9 +24,10 @@
             return $result;
         }
 
+
         /**
          * Consulta todas las incidencias de la BD.
-         * @return Todas las incidencias como objetos de un array o null en caso de error.
+         * @return array Todas las incidencias como objetos de un array o null en caso de error.
          */
         public function getAll() {
             $arrayResult = array();
@@ -39,6 +41,13 @@
             return $arrayResult;
         }
 
+
+        /**
+         * Consulta todas las incidencias de la BD en función de un campo y la dirección.
+         * @param campo El campo por el que ordenar las incidencias.
+         * @param tipo La dirección a la que se ordena (ascendentemente o descendentemente).
+         * @return array Todas las incidencias ordenadas como objetos en un array o null en caso de error.
+         */
         public function getAllOrdenadas($campo, $tipo) {
             $arrayResult = array();
             if ($result = $this->db->query("SELECT * FROM incidencias ORDER BY $campo $tipo")) {
@@ -51,10 +60,11 @@
             return $arrayResult;
         }
 
+
         /**
          * Consulta todas las incidencias de un usuario en concreto.
          * @param id_user el ID del usuario del que queremos buscar sus incidencias.
-         * @return Todas las incidencias como objetos de un array o null en caso de error.
+         * @return array Todas las incidencias como objetos de un array o null en caso de error.
          */
         public function getSelected($id_user) {
             $arrayReult = array();
@@ -68,6 +78,11 @@
             return $arrayResult;
         }
 
+
+        /**
+         * Actualiza un registro de la tabla incidencias de la BD.
+         * @return int El número de filas afectadas en la BD (1 si tuvo éxito ó 0 si no).
+         */
         public function update($id_incidencia,$fecha,$lugar,$equipo_afectado,$descripcion,$observaciones,$estado,$prioridad) {
             $this->db->query("UPDATE incidencias SET
                                                 fecha = '$fecha',
@@ -81,12 +96,22 @@
             return $this->db->affected_rows;
         }
 
+
+        /**
+         * Elimina un registro de la tabla incidencias de la BD
+         * @param id_incidencia El ID de la incidencia que se desea eliminar.
+         * @return int El número de filas afectadas en la BD (1 si tuvo éxito ó 0 si no).
+         */
         public function delete($id_incidencia) {
             $this->db->query("DELETE FROM incidencias WHERE id_incidencia = '$id_incidencia'");
             return $this->db->affected_rows;
         }
 
 
+        /**
+         * Añade un nuevo registro a la tabla incidencias pasándole como parámetros los diferentes campos de la tabla.
+         * @return int El número de filas afectadas en la BD (1 si tuvo éxito ó 0 si no).
+         */
         public function insert($id_user, $fecha, $lugar, $equipo_afectado, $descripcion, $observaciones, $estado) {
             $this->db->query("INSERT INTO incidencias
                                 VALUES (null, '$fecha','$lugar','$equipo_afectado','$descripcion','$id_user','$observaciones','$estado','media');");
@@ -94,6 +119,11 @@
         }
 
 
+        /**
+         * Realiza una búsqueda en la tabla incidencias de un texto en concreto.
+         * @param texto_busqueda el texto por el que hacer la búsqueda de campos.
+         * @return array Todas las incidencias como objetos de un array o null en caso de error.
+         */
         public function search($texto_busqueda) {
             $arrayResult = array();
 
@@ -116,6 +146,12 @@
             return $arrayResult;
         }
 
+        
+        /**
+         * Establece como cerrada el campo prioridad y estado de un registro de la tabla incidencias.
+         * @param id_incidencia El ID de la incidencia al que cambiar los valores.
+         * @return int El número de filas afectadas en la BD (1 si tuvo éxito ó 0 si no).
+         */
         public function cerrarIncidencia($id_incidencia) {
             $this->db->query("UPDATE incidencias SET
                                                     prioridad='cerrada',
--- models/user.php
+++ models/user.php
@@ -3,10 +3,21 @@
     class User {
         private $db;
 
+
+        /**
+         * Contructor. Establece la conexión con la BD y lo guarda en una variable.
+         */
         public function __construct() {
             $this->db = new mysqli('localhost','root','','accidentao');
         }
 
+
+        /**
+         * Busca en la tabla users un usuario en concreto. Si lo encuentra, crea las variables de sesión.
+         * @param usr El nombre de usuario.
+         * @param passwd La contraseña del usuario.
+         * @return bool True si lo encuentra y False si no lo encuentra.
+         */
         public function buscarUser($usr, $passwd) {
             $buscado = false;
             if ($result = $this->db->query("SELECT * FROM users WHERE nombre = '$usr' AND passwd = '$passwd'")) {
@@ -21,6 +32,12 @@
             return $buscado;
         }
 
+
+        /**
+         * Busca en la tabla users un registro por su ID.
+         * @param id_user El ID del usuario a buscar.
+         * @return object Un objeto con la información del usuario encontrado; o null si no lo encuentra.
+         */
         public function get($id_user) {
             if ($result = $this->db->query("SELECT * FROM users WHERE id_user = '$id_user'")) {
                 $result = $result->fetch_object();
@@ -31,6 +48,11 @@
             return $result;
         }
 
+
+        /**
+         * Consulta todos los registros de la tabla users de la BD.
+         * @return array Todas los usuarios como objetos en un array o null si no hay.
+         */
         public function getAll() {
             $arrayResult = array();
             if ($result = $this->db->query("SELECT * FROM users")) {
@@ -44,18 +66,36 @@
             return $arrayResult;
         }
 
+
+        /**
+         * Realiza una inserción de un registro a la tabla users de la BD, pasándole como
+         * parámetros los diferentes campos del registro.
+         * @return int El número de filas afectadas en la BD (1 si tuvo éxito ó 0 si no).
+         */
         public function insert($nombre, $passwd, $correo, $rol) {
             $this->db->query("INSERT INTO users
                                 VALUES (null, '$nombre','$correo','$passwd','$rol')");
             return $this->db->affected_rows;
         }
 
+
+        /**
+         * Elimina un registro de la tabla usera de la BD.
+         * @param id_user El ID del usuario a borrar.
+         * @return int El número de filas afectadas en la BD (1 si tuvo éxito ó 0 si no).
+         */
         public function delete($id_user) {
             $this->db->query("DELETE FROM users WHERE id_user = '$id_user'");
 
             return $this->db->affected_rows;
         }
 
+
+        /**
+         * Actualiza un registro de la tabla users de la BD, pasándole como
+         * parámetros los diferentes campos del registro.
+         * @return int El número de filas afectadas en la BD (1 si tuvo éxito ó 0 si no).
+         */
         public function update($id_user,$nombre,$passwd,$correo,$rol) {
             $this->db->query("UPDATE users SET
                                             id_user = '$id_user',
